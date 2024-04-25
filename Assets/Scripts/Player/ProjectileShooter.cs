85c88e7f091415b0448aa77cf959c89efc6ff003
Ali107 <Alialawadhi2014bh@gmail.com>
2024-04-25 14:49:18 +0300

    Added support for infinite ammo.

--- Assets/Scripts/Player/ProjectileShooter.cs
+++ Assets/Scripts/Player/ProjectileShooter.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class ProjectileShooter : MonoBehaviour
 {
     public GameObject projectilePrefab;
+    public bool infiniteAmmo = true;
     public uint ammo = 16;
     public float shootPower = 16;
     PlayerControllerScript playerControllerScript;
@@ -20,15 +21,14 @@ public class ProjectileShooter : MonoBehaviour
         {
             if (Input.GetKeyDown(KeyCode.Mouse0)) 
             {
-                if (ammo > 0)
+                if (ammo > 0 || infiniteAmmo)
                 {
-                    ammo--;
+                    if (!infiniteAmmo) ammo--;
                     Transform camAnchorTransform = playerControllerScript.camAnchor.transform;
                     GameObject instProjectile = Instantiate(projectilePrefab, camAnchorTransform.position + (camAnchorTransform.forward * gameObject.GetComponent<CapsuleCollider>().radius), new Quaternion());
                     Rigidbody rb;
                     if (instProjectile.TryGetComponent<Rigidbody>(out rb))
                         rb.velocity = camAnchorTransform.forward * shootPower;
-                    Debug.Log("Ammo Remaining = " + ammo);
                 }
             }
         }
