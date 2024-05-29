46c94a500b04c63b30c6d12e37ce03a830f2c7a1
Ali107 <Alialawadhi2014bh@gmail.com>
2024-05-29 16:16:58 +0300

    Added comments to Projectile Shooter script.

--- Assets/Scripts/Player/ProjectileShooter.cs
+++ Assets/Scripts/Player/ProjectileShooter.cs
@@ -27,29 +27,27 @@ public class ProjectileShooter : MonoBehaviour
         {
             if ((!requiresChargeUp && Input.GetKeyDown(KeyCode.Mouse0)) || (requiresChargeUp && timeCharging >= chargeUpTime && Input.GetKeyUp(KeyCode.Mouse0)))
             {
-                if (ammo > 0 || infiniteAmmo)
+                if (ammo > 0 || infiniteAmmo) // Checks if player has ammo, ignores check if infiniteAmmo is true
                 {
-                    timeCharging = 0f;
-                    if (!infiniteAmmo) ammo--;
-                    Transform camAnchorTransform = cam.transform;
-                    GameObject instProjectile = Instantiate(projectilePrefab, camAnchorTransform.position + (camAnchorTransform.forward * gameObject.GetComponent<CapsuleCollider>().radius), new Quaternion());
+                    timeCharging = 0f; // Resets charge to zero
+                    if (!infiniteAmmo) ammo--; // consumes ammo if infiniteAmmo is false
+
+                    // Spawns projectile at player camera position
+                    Transform camTransform = cam.transform;
+                    GameObject instProjectile = Instantiate(projectilePrefab, camTransform.position + (camTransform.forward * gameObject.GetComponent<CapsuleCollider>().radius), new Quaternion());
                     Rigidbody rb;
-                    if (instProjectile.TryGetComponent<Rigidbody>(out rb))
-                        rb.velocity = camAnchorTransform.forward * shootPower;
+                    if (instProjectile.TryGetComponent<Rigidbody>(out rb)) // if projectile has rigid body, it will cause the projectile to shoot and move in camera's direction
+                        rb.velocity = camTransform.forward * shootPower;
                 }
             }
-            if (requiresChargeUp)
+            if (requiresChargeUp) // Script below will only occur if projectile shooter requires the player to charge up before shooting
             {
-                if (Input.GetKey(KeyCode.Mouse0))
-                {
-                    timeCharging += Time.deltaTime;
-                }
+                if (Input.GetKey(KeyCode.Mouse0)) timeCharging += Time.deltaTime; // Charges up the projectile shooter
 
-                fpc.canSprint = !Input.GetKey(KeyCode.Mouse0);
-                fpc.zoomFOV = Mathf.Lerp(fpc.fov, 60, timeCharging / chargeUpTime);
+                fpc.canSprint = !Input.GetKey(KeyCode.Mouse0); // Disables sprinting when charging up shooter
+                fpc.zoomFOV = Mathf.Lerp(fpc.fov, 60, timeCharging / chargeUpTime); // Changes FOV based on the current shooter charge
 
                 Debug.Log(timeCharging);
-
             }
         }
     }
