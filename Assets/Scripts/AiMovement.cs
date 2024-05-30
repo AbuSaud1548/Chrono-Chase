36f5f8dbbde372f5ce541c629e606549ada70b43
hjh951 <hassan951478@gmail.com>
2024-05-30 23:35:21 +0300

    added attack animation
    attack animation

--- Assets/Scripts/AiMovement.cs
+++ Assets/Scripts/AiMovement.cs
@@ -7,18 +7,53 @@ public class AiMovement : MonoBehaviour
 {
     Transform PlayerMovement;
     NavMeshAgent Enemy;
+    public Animator animator;  
+    public float closeDistance = 5.0f;  
+    private bool isClose = false;
+    private bool isAnimating = false;
+
     
-    // Start is called before the first frame update
     void Start()
     {
         Enemy = GetComponent<NavMeshAgent>();
         PlayerMovement = GameObject.Find("FirstPersonController").transform;
+        animator = GetComponent<Animator>();  
     }
 
-    // Update is called once per frame
+   
     void Update()
     {
-        Enemy.destination = PlayerMovement.position;
-        
+        if (!isAnimating)
+        {
+            Enemy.destination = PlayerMovement.position;
+
+            
+            float distanceToPlayer = Vector3.Distance(Enemy.transform.position, PlayerMovement.position);
+
+            
+            if (distanceToPlayer < closeDistance)
+            {
+                if (!isClose)
+                {
+                    isClose = true;
+                    animator.SetBool("isClose", true);  
+                    StartCoroutine(ResetIsCloseAfterAnimation());  
+                }
+            }
+        }
+    }
+
+    IEnumerator ResetIsCloseAfterAnimation()
+    {
+        isAnimating = true;
+
+       
+        AnimatorStateInfo stateInfo = animator.GetCurrentAnimatorStateInfo(0);
+        yield return new WaitForSeconds(stateInfo.length);
+
+        isClose = false;
+        animator.SetBool("isClose", false);  
+
+        isAnimating = false;
     }
 }
