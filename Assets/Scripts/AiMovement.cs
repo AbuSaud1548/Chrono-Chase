52dfa4b7ee866afc04b269f95030acd3be16195b
hjh951 <hassan951478@gmail.com>
2024-05-31 20:25:12 +0300

    added player damage
    added player damage  and tested the medkit

--- Assets/Scripts/AiMovement.cs
+++ Assets/Scripts/AiMovement.cs
@@ -1,5 +1,4 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 
@@ -10,82 +9,96 @@ public class AiMovement : MonoBehaviour
     public Animator animator;
     public float closeDistance = 3.0f;
     public float sightRange = 40.0f;
-    private bool isClose = false;
     private bool isAnimating = false;
-    private float attackTimer = 0;
+    private SwordCollider swordCollider;
 
-    
     void Start()
     {
         Enemy = GetComponent<NavMeshAgent>();
         PlayerMovement = GameObject.Find("FirstPersonController").transform;
-        animator = GetComponent<Animator>();  
+        animator = GetComponent<Animator>();
+        swordCollider = GetComponentInChildren<SwordCollider>(); // Find the SwordCollider in children
+        swordCollider.DisableCollider(); // Ensure the collider starts disabled
     }
 
-   
     void Update()
     {
         float distanceToPlayer = Vector3.Distance(Enemy.transform.position, PlayerMovement.position);
 
-        if (distanceToPlayer < sightRange)
-        {
-            Enemy.destination = PlayerMovement.position;
-        }
-        else 
-        {
-            Enemy.destination = Enemy.transform.position;
-        }
-
         if (!isAnimating)
         {
-
-            isClose = distanceToPlayer < closeDistance;
-
-            if (isClose)
+            if (distanceToPlayer < sightRange && distanceToPlayer > closeDistance)
+            {
+                Debug.Log("Chasing Player");
+                Enemy.isStopped = false; // Ensure the enemy can move
+                Enemy.destination = PlayerMovement.position;
+                animator.SetBool("isClose", false);
+            }
+            else if (distanceToPlayer <= closeDistance)
             {
                 if (!animator.GetBool("isClose"))
                 {
+                    Debug.Log("Preparing to Attack");
                     animator.SetBool("isClose", true);
+                    StartCoroutine(AttackRoutine());
                 }
             }
-
-            if (animator.GetBool("isClose"))
+            else
             {
-                attackTimer += Time.deltaTime;
-            }
-
-            if (attackTimer >= 3f)
-            {
+                Debug.Log("Player out of range");
+                Enemy.isStopped = true; // Stop the enemy if out of sight range
+                Enemy.destination = Enemy.transform.position;
                 animator.SetBool("isClose", false);
-                attackTimer = 0;
             }
-            Debug.Log(animator.GetBool("isClose"));
-
-            //if (distanceToPlayer < closeDistance)
-            //{
-            //    if (!isClose)
-            //    {
-            //        isClose = true;
-            //        animator.SetBool("isClose", true);  
-            //        StartCoroutine(ResetIsCloseAfterAnimation());  
-            //    }
-            //}
         }
     }
 
-
-
-    IEnumerator ResetIsCloseAfterAnimation()
+    IEnumerator AttackRoutine()
     {
         isAnimating = true;
+        Enemy.isStopped = true; // Stop the enemy's movement
+        Enemy.velocity = Vector3.zero; // Ensure no sliding
 
-       
-        AnimatorStateInfo stateInfo = animator.GetCurrentAnimatorStateInfo(0);
-        yield return new WaitForSeconds(stateInfo.length);
+        while (animator.GetBool("isClose"))
+        {
+            Debug.Log("Attacking Player");
 
-        isClose = false;
-        animator.SetBool("isClose", false);  
+            // Enable the sword collider at the start of the attack
+            swordCollider.EnableCollider();
+
+            // Wait for the duration of the attack animation
+            yield return new WaitForSeconds(1.0f);
+
+            float distanceToPlayer = Vector3.Distance(Enemy.transform.position, PlayerMovement.position);
+            if (distanceToPlayer > closeDistance)
+            {
+                Debug.Log("Player out of attack range");
+                animator.SetBool("isClose", false);
+                isAnimating = false;
+                Enemy.isStopped = false; // Resume the enemy's movement
+                yield break;
+            }
+
+            // Disable the sword collider after the attack
+            swordCollider.DisableCollider();
+
+            yield return new WaitForSeconds(2.0f); // Time between attacks
+        }
 
         isAnimating = false;
+        Enemy.isStopped = false; // Resume the enemy's movement
+    }
+
+    // These methods can also be called directly from animation events if required
+    public void EnableCollider()
+    {
+        Debug.Log("EnableCollider called");
+        swordCollider.EnableCollider();
+    }
+
+    public void DisableCollider()
+    {
+        Debug.Log("DisableCollider called");
+        swordCollider.DisableCollider();
     }
 }
---
+++ Assets/Scripts/Gameplay/SwordColider.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+
+public class SwordCollider : MonoBehaviour
+{
+    public float damageAmount = 10.0f; // Damage dealt by the sword
+
+    void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Sword hit " + other.name);
+        CharacterHealthSystem healthSystem = other.GetComponent<CharacterHealthSystem>();
+        if (healthSystem != null)
+        {
+            Debug.Log("Dealing damage to " + other.name);
+            healthSystem.DealDamage(damageAmount);
+        }
+    }
+
+    // Called by the parent to enable the sword collider during attack
+    public void EnableCollider()
+    {
+        Debug.Log("Sword Collider Enabled");
+        GetComponent<Collider>().enabled = true;
+    }
+
+    // Called by the parent to disable the sword collider after attack
+    public void DisableCollider()
+    {
+        Debug.Log("Sword Collider Disabled");
+        GetComponent<Collider>().enabled = false;
+    }
+}
