34cec990e75c382e17114deb9764a4637e121399
Ali107 <Alialawadhi2014bh@gmail.com>
2024-06-02 20:06:32 +0300

    Fixed all the issues in Pause Menu.

--- Assets/Scripts/PauseMenu.cs
+++ Assets/Scripts/PauseMenu.cs
@@ -8,12 +8,9 @@ public class PauseMenu : MonoBehaviour
 {
     public GameObject PausePanel;
     private bool isPaused = false;
-    public GameObject PauseSound;
-    private AudioSource PauseSound1;
     private void Start()
     {
         PausePanel.SetActive(false);
-        AudioSource PauseSound1 = PauseSound.GetComponent<AudioSource>();
     }
     private void Update()
     {
@@ -28,6 +25,14 @@ public class PauseMenu : MonoBehaviour
                 PauseGame();
             }
         }
+
+        if (isPaused)
+        {
+            if (Input.GetKeyDown(KeyCode.Return)) returntoMenu();
+
+            Cursor.lockState = CursorLockMode.None;
+            Cursor.visible = true;
+        }
     }
 
     private void PauseGame()
@@ -35,30 +40,22 @@ public class PauseMenu : MonoBehaviour
         Time.timeScale = 0; // Set the time scale to 0 to freeze the game
         isPaused = true;
         PausePanel.SetActive(true);
-        PauseSound1.Pause();
 
-
-        // Show the pause menu UI or perform any other necessary actions
-        Debug.Log("Game paused");
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
     }
 
-    private void ResumeGame()
+    public void ResumeGame()
     {
         Time.timeScale = 1; // Set the time scale back to 1 to resume the game
         isPaused = false;
         PausePanel.SetActive(false);
-        // Hide the pause menu UI or perform any other necessary actions
-        Debug.Log("Game resumed");
-        PauseSound1.Play();
+
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
     }
     public void returntoMenu()
     {
         SceneManager.LoadScene("MainMenuScene");
-
-    }
-    public void exit()
-    {
-        Application.Quit();
-
     }
 }
